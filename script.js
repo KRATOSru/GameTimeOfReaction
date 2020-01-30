567189a991a74a5187c3d0f9287a020a67431008
v-vlkart <vovan198080@inbox.ru>
2020-01-30 07:31:26 +0300

    Time off Reaction part2

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 var startTime = new Date().getTime();
 
+var makeShapeVisible = function(){
+    document.getElementById("shape").style.display = "block"
+    startTime = new Date().getTime();
+}
+
+makeShapeVisible();
+
 document.getElementById("shape").onclick = function () {
+    var shape = document.getElementById("shape");
+    var top = Math.random()*400;
+    var left = Math.random()*700;
+    shape.style.display = "none";
+    shape.style.top = top + "px";
+    shape.style.left = left + "px";
     var finishTime = new Date().getTime();
-    document.getElementById("shape").style.display = "none"
     var reactionTime = (finishTime-startTime) / 1000;
     document.getElementById("reactionTime").innerHTML = reactionTime + " seconds."
+    setTimeout(makeShapeVisible,Math.random()*3000);
 }
